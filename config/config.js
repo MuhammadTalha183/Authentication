c822b6bd65231f65c433ae055b2f92707733aa34
M-Talha183 <ullahehsan016@gmail.com>
2026-06-27 18:55:07 +0500

    Add datbase connection file

--- config/config.js
+++ config/config.js
@@ -2,10 +2,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
+
+if (!process.env.DATABASE_URL){
+    throw new Error("DATABASE_URL is not defined in the environment variables");
+}
 const config = {
-  port: process.env.PORT || 3000,
   DATABASE_URL: process.env.DATABASE_URL,
-//   jwtSecret: process.env.JWT_SECRET 
 };
 
 
